5962972a630ee3e891f43d50ceba4e24050c7597
SULAIMAN KHALFAN MOHAMMED AL HUDAIFI <SKMhudaifi@rca.gov.om>
2024-12-18 13:41:30 +0400

    complete the login page

--- login-form/src/Components/InputField.jsx
+++ login-form/src/Components/InputField.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react"
+
 const InputField = ({type, placeholder,
     icon}) => {
+        // state to toggle password visibility
+    const [isPasswordShown, setIsPasswordShown] = useState(false);
     return (
         <div className="input-wrapper">
-        <input type={type} placeholder={placeholder}
-        className="input-field"required />
+        <input 
+            type={isPasswordShown ? 'text' : type}
+            placeholder={placeholder}
+            className="input-field"
+            required
+        />
         <i className="material-symbols-rounded">{icon}</i>
         {type === 'password' && (
-            <i className="material-symbols-rounded eye-icon">
-                visibility_off
+            <i onClick={() => setIsPasswordShown(prevState => !prevState)} className="material-symbols-rounded eye-icon">
+                {isPasswordShown ? 'visibility' : 'visibility_off'}
             </i>
         )} 
        </div>
